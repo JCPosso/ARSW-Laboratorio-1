4f44d9b21bfcda06694b9c423d4a854f693c3e63
JC <juancamiloposso01@gmail.com>
2021-08-12 09:57:08 -0500

    Implementacion ThreadBlackList

---
+++ src/main/java/edu/eci/arsw/blacklistvalidator/ThreadBlackList.java
@@ -0,0 +1,37 @@
+package edu.eci.arsw.blacklistvalidator;
+
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
+import java.util.LinkedList;
+
+public class ThreadBlackList extends Thread {
+    private static final int BLACK_LIST_ALARM_COUNT=10;
+    private int a,b,ocurrences;
+    private String Host;
+    LinkedList<Integer> blackListOcurrences=new LinkedList<>();
+    private int checkedListsCount;
+    private int ocurrencesCount;
+    HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
+    public ThreadBlackList(int a , int b, String Host){
+        this.a=a;
+        this.b=b;
+        this.Host=Host;
+        this.checkedListsCount=0;
+        this.ocurrencesCount=0;
+    }
+    public void run() {
+        for ( int i = a; i < b && checkedListsCount < BLACK_LIST_ALARM_COUNT; i++ ) {
+            checkedListsCount++;
+            if ( skds.isInBlackListServer( i, Host ) ) {
+                blackListOcurrences.add( i );
+                ocurrencesCount++;
+            }
+        }
+    }
+    public LinkedList<Integer> getBlackListOcurrences(){
+            return blackListOcurrences;
+    }
+    public  int getOcurrencesCount(){
+            return ocurrencesCount;
+    }
+}
